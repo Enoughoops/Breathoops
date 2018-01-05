02b1c9c21378bd8d9632cec321727e01a11defbe
linmo <wangjiachen0609@163.com>
2018-01-05 15:42:58 +0800

    Initial Commit

---
+++ Breath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Breath
+//
+//  Created by LinMo on 2018/1/5.
+//  Copyright © 2018年 LinMo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
